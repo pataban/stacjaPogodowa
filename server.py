a1e193985f61a34238658b2404f14a8ba518658d
pataban <brajerpatryk@gmail.com>
2022-01-17 19:50:00 +0100

    added data collection

--- server.py
+++ server.py
@@ -12,10 +12,10 @@ topicRecieve = "sensorData"
 data={"action":"measurement",
     "sensorId":123456789}   
     #dla None dotyczy karzdy sensor #ewentualne dodatkowe klucze, wartosci
-dbConn,sensors,weather=None,None,None
+dbEngine,sensors,weather=None,None,None
 
 
-def makeDB():
+def openDB():
     engine = db.create_engine('sqlite:///weather.db')
     dbConnection = engine.connect()
     metadata = db.MetaData()
@@ -33,10 +33,11 @@ def makeDB():
         )
 
     metadata.create_all(engine)
-    return dbConnection,sensors,weather
+    return engine,sensors,weather
 
 
 def chkDefaultSensors():    #gwarantuje istnienie sensorow 123456789 i 987654321
+    dbConn=dbEngine.connect()
     query=db.select([sensors]).where(sensors.columns.id == "123456789")
     resultProxy = dbConn.execute(query)
     resultSet = resultProxy.fetchall()
@@ -50,9 +51,11 @@ def chkDefaultSensors():    #gwarantuje istnienie sensorow 123456789 i 987654321
     if (len(resultSet)==0):
         query = db.insert(sensors).values(id="987654321")
         resultProxy = dbConn.execute(query)
+    dbConn.close()
 
 
 def prtDB():
+    dbConn=dbEngine.connect()
     query=db.select([sensors])
     resultProxy = dbConn.execute(query)
     resultSet = resultProxy.fetchall()
@@ -64,19 +67,47 @@ def prtDB():
     resultSet = resultProxy.fetchall()
     print("Weather:")
     print(resultSet)
+    dbConn.close()
 
 
 
-def onRecieve(data):    #TODO   przetwarzanie odebranych danych
+def verifySensor(sensorId):
+    if((sensorId=="") or (sensorId=="000")):
+        return False
+
+    dbConn=dbEngine.connect()
+    query=db.select([sensors]).where(sensors.columns.id == sensorId)
+    resultSet = dbConn.execute(query).fetchall()
+    dbConn.close()
+
+    if (len(resultSet)==0):
+        return False
+    else:
+        return True
+
+
+
+def onRecieve(data):    #uruchamiany asynchronicznie na innym watku
     print(f"rec: {data}")
+    
+    if(not verifySensor(data["sensorId"])):
+        print("User not valid")
+        return
+
+    dbConn=dbEngine.connect()
+    query = db.insert(weather).values(sensorId=data["sensorId"]
+        ,temp=data["temp"],hum=data["hum"],press=data["press"],date=data["date"])
+    resultProxy = dbConn.execute(query)
+    dbConn.close()
+
 
 
 if __name__ == '__main__':
-    dbConn,sensors,weather=makeDB()
+    dbEngine,sensors,weather=openDB()
     chkDefaultSensors()
     conn=ConnectionHandler(serverId,addr,port,topicSend,topicRecieve,onRecieve)
     prtDB()
-    
+
     while(True):
         time.sleep(1)
         #choice=int(input("Twoj wybr: "))
