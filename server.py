bd1590d1584d132f066cd9378f204ff5728e2c9e
pataban <brajerpatryk@gmail.com>
2022-01-18 00:19:32 +0100

    added UI for data view

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import time
 import sqlalchemy as db
 import pandas as pd
+import datetime
 
 from ConnectionHandler import ConnectionHandler
 
@@ -14,6 +15,9 @@ data={"action":"measurement",
     #dla None dotyczy karzdy sensor #ewentualne dodatkowe klucze, wartosci
 dbEngine,sensors,weather=None,None,None
 
+
+def getTime():
+    return datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
 
 def openDB():
     engine = db.create_engine('sqlite:///weather.db')
@@ -61,13 +65,10 @@ def prtDB():
     resultSet = resultProxy.fetchall()
     print("Sensors:")
     print(resultSet)
-
-    query=db.select([weather])
-    resultProxy = dbConn.execute(query)
-    resultSet = resultProxy.fetchall()
-    print("Weather:")
-    print(resultSet)
     dbConn.close()
+
+    printData(getData())
+    
 
 
 
@@ -101,16 +102,88 @@ def onRecieve(data):    #uruchamiany asynchronicznie na innym watku
     dbConn.close()
 
 
+def printData(data):
+    for i in range(0,len(data)):
+        data[i]={"sensorId":data[i][0],"temp":data[i][1]
+        ,"hum":data[i][2],"press":data[i][3],"date":data[i][4]}
+    df=pd.DataFrame(data)
+    if len(df)>0:
+        print(df)
+    else:
+        print("No Data")
+
+
+def getData(sensorId=None,tempMin=None,tempMax=None,date=None):
+    query=db.select([weather])
+    if(sensorId!=None):
+        query=query.where(weather.columns.sensorId == sensorId)
+    if(tempMin!=None and tempMax!=None):
+        query=query.where(weather.columns.temp >= tempMin)
+        query=query.where(weather.columns.temp < tempMax)
+    if(date!=None):
+        query=query.where(weather.columns.date.contains(date))
+    dbConn=dbEngine.connect()
+    resultProxy = dbConn.execute(query)
+    res = resultProxy.fetchall()
+    dbConn.close()
+    return res
+
+
+
+def menu():
+    print("")
+    print("Menu:")
+    print("1. Przegladaj dane.")
+    print("2. Ustaw alarm.")
+    choice=int(input("Twoj wybr: "))
+    if(choice==1):
+        menuDane()
+    elif(choice==2):
+        menuAlarm()
+    return menu()
+
+def menuDane():
+    print("")
+    print("Wybor danych:")
+    print("1. Wszystkie.")
+    print("2. Dla danego sensora.")                 #sensorId="123456789"
+    print("3. Dla danego przedzialu temperatur.")   #tempMin=21.1,tempMax=22.8
+    print("4. Dla danego okresu czasu.")            #date="17.01.2022 22"
+    choice=int(input("Twoj wybr: "))
+    if(choice==1):
+        printData(getData())
+    elif(choice==2):
+        printData(getData(sensorId=input("Podaj Id sensora: ")))
+    elif(choice==3):
+        printData(getData(tempMin=float(input("Podaj temperature minimalna: "))
+        ,tempMax=float(input("Podaj temperature maksymalna: "))))
+    elif(choice==4):
+        printData(getData(date=input("Podaj okres: ")))
+    return menu()
+
+def menuAlarm():
+    pass#conn.send(data)
+
 
 if __name__ == '__main__':
     dbEngine,sensors,weather=openDB()
     chkDefaultSensors()
     conn=ConnectionHandler(serverId,addr,port,topicSend,topicRecieve,onRecieve)
+    
     prtDB()
 
+    #make custom data
+    """dbConn=dbEngine.connect()    
+    query = db.insert(weather).values(sensorId="987654321"
+        ,temp="22.6",hum="90.5",press="1030.0",date=getTime())
+    resultProxy = dbConn.execute(query)
+    dbConn.close()"""
+
+
     while(True):
-        time.sleep(1)
-        #choice=int(input("Twoj wybr: "))
-        #conn.send(data)
+        menu()
+        time.sleep(3)
+        
+        
 
 
